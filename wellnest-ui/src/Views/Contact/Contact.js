e531085d6bf827c938b38d1591db5f0ba003e43c
Altriti <altritgallapeni@gmail.com>
2021-06-02 17:19:25 +0200

    Contact-ui

--- wellnest-ui/src/Views/Contact/Contact.js
+++ wellnest-ui/src/Views/Contact/Contact.js
@@ -9,7 +9,7 @@ export class Contact extends Component{
             <div class="background">
 			<div class="inner">
 				<form action="">
-					<h3>Contact Us</h3>
+					<h3 className='cu-text'>Contact Us</h3>
 					<label class="form-group">
 						<input type="text" class="f-dezign" required/>
 						<span>Name</span>
@@ -21,11 +21,11 @@ export class Contact extends Component{
 						<span class="border"></span>
 					</label>
 					<label class="form-group" >
-						<textarea name="" id="" class="f-dezign" required></textarea>
+						<textarea name="" id="ta" class="f-dezign" required></textarea>
 						<span for="">Message</span>
 						<span class="border"></span>
 					</label>
-					<button>Submit </button>
+					<button className='button'>Submit </button>
 				</form>
 			</div>
 		</div>
