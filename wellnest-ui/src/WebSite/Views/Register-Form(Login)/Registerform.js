4910ca146ecfec2291b86097dade86a91f01e115
edisonarexha <rexhaedisona@gmail.com>
2021-06-30 23:09:22 +0200

    Post Users modified

--- wellnest-ui/src/WebSite/Views/Register-Form(Login)/Registerform.js
+++ wellnest-ui/src/WebSite/Views/Register-Form(Login)/Registerform.js
@@ -2,105 +2,88 @@ import React, { Component, useState } from "react";
 import "./Registerform.css";
 
 class Registerform extends Component {
+  state = {
+    name: null,
+    surname: null,
+    email: null,
+    userkey: null,
+  };
+  setName = (event) => {
+    this.setState({ name: event.target.value });
+  };
 
-    state = {
-      Name: "",
-      Email: "",
-      Username: "",
-      Password: "",
-      Role: "",
-    };
-    setName = (event) => {
-      this.setState({Name: event.target.value});
-    }
-  
-    setEmail = (event) => {
-      this.setState({Email: event.target.value});
-    }
-    
-    setUsername = (event) => {
-      this.setState({Username: event.target.value});
-    }
-    setPassword = (event) => {
-      this.setState({Password: event.target.value});
-    }
-    setRole = (event) => {
-      this.setState({Role: event.target.value});
-    }
-  
-    addNewUser = () => {
-      console.log("test")
-      fetch('https://localhost:5000/api/Users',{
-          method:'POST',
-          headers:{'Accept':'application/json',
-          'Content-Type':'application/json;charset=utf-8'},
-          body: JSON.stringify(this.state)
-      }).then(() => {
+  setEmail = (event) => {
+    this.setState({ email: event.target.value });
+  };
+
+  setSurname = (event) => {
+    this.setState({ surname: event.target.value });
+  };
+  setUserkey = (event) => {
+    this.setState({ userkey: event.target.value });
+  };
+
+
+  addNewUser = () => {
+    console.log("test");
+    fetch('http://localhost:5000/API/User', {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(this.state),
+    }).then(() => {
       //kena me shku nhome
-      })
-    }
-  
-    handleFileSelected = (event) => {
-      event.preventDefault();
-    }
-  
+    });
+  };
+
+  handleFileSelected = (event) => {
+    event.preventDefault();
+  };
 
   render() {
     return (
       <div className="container">
-
         <div className="pannel">
           <div className="wrapper">
-            <form >
+            <form>
               <div className="input-fields">
-                <div className="field">
-                  <input
-                    type="text"
-                    value={this.state.Name} 
-                    onChange={this.setName}
-                    placeholder="FirstName..."
-                  />
-                </div>
-                <div className="field">
-                  <input
-                    type="username"
-                    value={this.state.Username} 
-                    onChange={this.setUsername}
-                    placeholder="Username..."
-                  />
-                </div>
-                <div className="field">
-                  <input
-                    type="email"
-                    value={this.state.Email}
-                    onChange={this.setEmail}
-                    placeholder="Email..."
-                  />
-                </div>
-                <div className="field">
-                  <input
-                    type="password"
-                    value={this.state.Password}
-                    onChange={this.setPassword}
-                    placeholder="Password..."
-                  />
-                </div>
-                <div className="field" >
-                  <div className="roles">
-
-                  <select onChange={this.setRole} defaultValue="Select Role" style={{width: '70%'}}>
-                    <option defaultValue>Select Role</option>
-                    <option value="male">Pacient</option>
-                    <option value="female">Doctor</option>
-                  </select>
-                  </div>
-                </div>
+              <div className="field">
+                <input
+                  type="text"
+                  value={this.state.name}
+                  onChange={this.setName}
+                  placeholder="FirstName..."
+                />
               </div>
-              <div className="centered">
-                <button 
-                onClick={() => this.addNewUser()}
-                className="button">Sign up</button>
+              <div className="field">
+                <input
+                  type="surname"
+                  value={this.state.surname}
+                  onChange={this.setSurname}
+                  placeholder="surname..."
+                />
               </div>
+              <div className="field">
+                <input
+                  type="email"
+                  value={this.state.email}
+                  onChange={this.setEmail}
+                  placeholder="Email..."
+                />
+              </div>
+              <div className="field">
+                <input
+                  type="Userkey"
+                  value={this.state.userkey}
+                  onChange={this.setUserkey}
+                  placeholder="userkey..."
+                />
+              </div>
+              
+              </div><div className="button" onClick={() => this.addNewUser()} type="button">
+            Save</div>
             </form>
           </div>
         </div>
